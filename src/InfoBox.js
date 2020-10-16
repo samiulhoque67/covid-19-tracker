47b641af9cff7f937559b25b868bfeb0d9a4dadb
samiul <samiulhoque67@gmail.com>
2020-10-16 12:52:40 +0600

    header created

---
+++ src/InfoBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Card,CardContent,Typography} from "@material-ui/core";
+
+function InfoBox({title,cases,total}){
+
+
+
+	return(
+<div>
+<Card>
+<CardContent>
+{/*title */}
+<Typography color="textSecondary">
+{title}
+</Typography>
+<h2 className="infoBox__cases">{cases}</h2>
+<Typography className="infoBox__total" color="textSecondary">
+{total} total
+</Typography>
+
+
+
+</CardContent>
+</Card>
+</div>
+
+
+
+		)
+}
+export default InfoBox;
---
+++ src/Map.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card,CardContent,Typography} from "@material-ui/core";
+
+function Map(){
+
+
+
+	return(
+<div>
+<h1>I am map</h1>
+</div>
+
+
+
+		)
+}
+export default Map;
